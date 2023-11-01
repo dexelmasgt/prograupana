bae30846b52ca8fa5ac65f41503d45833ab07c8e
dexelmas <dmas.dj@gmail.com>
2023-11-01 07:04:22 -0600

    Revisión de ortografía

--- conectardb.php
+++ conectardb.php
@@ -1,24 +1,24 @@
 <?php
-//Ejemplo de conexion a base de datos MySQL con PHP.
+//Ejemplo de conexión a base de datos MySQL con PHP.
 
-//Guardamos en viariables lo parametros de la base de datos
+//Guardamos en viariables los parámetros de la base de datos
 $usuario = "root";
 $password = "";
 $servidor = "localhost";
 $basededatos = "prograupana";
 
-//Creamos la conexion a la base de datos. Usamos la funcion mysqli_connect()
+//Creamos la conexión a la base de datos. Usamos la función mysqli_connect()
 $conexion = mysqli_connect($servidor, $usuario, $password) or die ("No se ha podido conectar");
 
-//Despues de que creamos la conexion seleccionamos la base de datos
+//Después de que creamos la conexión seleccionamos la base de datos
 $db = mysqli_select_db($conexion, $basededatos) or die ("No se encontro la base de datos");
 
-//Realizamos la consulta y guardamos el contenido en una variable resultado
+//Realizamos la consulta y guardamos el contenido en una variable $resultado
 $consulta = "SELECT * FROM alumno";
 $resultado = mysqli_query($conexion, $consulta) or die ("Algo salio mal en la consulta");
 
 //Mostramos el resultado
-//ENCABEZADO
+//Encabezado de la tabla
 echo "<table border='1'>";
     echo "<tr>";
         echo "<th>ID</th>";
@@ -28,7 +28,7 @@ echo "<table border='1'>";
         echo "<th>Fecha de nacimiento</th>";
     echo "</tr>";
 
-    //Ciclo para mostrar el contenido de la base de datos
+    //Ciclo para mostrar el contenido de la base de datos en la tabla
     while($columna = mysqli_fetch_array($resultado)){
         echo "<tr>";
             echo "<td>" . $columna['id_alumno'] . "</td><td>" . $columna['nombre_alumno']
@@ -39,6 +39,6 @@ echo "<table border='1'>";
 
 echo "</table>";
 
-//Cerramos nuestra conexion que abrimos arriba
+//Cerramos nuestra conexión a la base de datos
 mysqli_close($conexion);
 ?>
